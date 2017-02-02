3a31ffd858939d4ea4821a97ef58ec8e203c4d62
Lilians <damien.claras@gmail.com>
2017-02-02 17:00:09 +0100

    correct use of http file

--- src/CoreBundle/Controller/DefaultController.php
+++ src/CoreBundle/Controller/DefaultController.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class DefaultController extends BaseController
 {
@@ -19,7 +20,7 @@ class DefaultController extends BaseController
      * )
      *
      * @Rest\View(statusCode=Response::HTTP_OK)
-     * @Rest\GET("/")
+     * @Rest\Get("/")
      */
     public function indexAction()
     {
--- src/CoreBundle/Controller/UserController.php
+++ src/CoreBundle/Controller/UserController.php
@@ -14,7 +14,6 @@ use CoreBundle\Entity\User;
 use CoreBundle\Entity\Credentials;
 use CoreBundle\Form\Type\CredentialsType;
 use CoreBundle\Controller\BaseController;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class UserController extends BaseController
 {
