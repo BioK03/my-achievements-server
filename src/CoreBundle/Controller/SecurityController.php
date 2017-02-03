1b804f1e8ee3718d628030f85125b8a6a91167f7
Lilians <damien.claras@gmail.com>
2017-02-03 12:44:36 +0100

    add first step for google OAuth

--- src/CoreBundle/Controller/SecurityController.php
+++ src/CoreBundle/Controller/SecurityController.php
@@ -14,6 +14,7 @@ use CoreBundle\Entity\User;
 use CoreBundle\Entity\Credentials;
 use CoreBundle\Form\Type\CredentialsType;
 use CoreBundle\Controller\BaseController;
+use Google_Client;
 
 class SecurityController extends BaseController
 {
@@ -118,5 +119,99 @@ class SecurityController extends BaseController
         } else {
             return $this->ok(false);
         }
+    }
+
+    /**
+     * Redirect to google OAuth
+     * Return :
+     * ['message' => auth url]
+     *
+     * @ApiDoc(
+     *  description="Login",
+     *  section="1-Security"
+     * )
+     *
+     * @Rest\Get("/googleredirect")
+     */
+    public function getGoogleRedirectAction(Request $request)
+    {
+        $client = new Google_Client();
+        $client->setAuthConfigFile(__DIR__.'/../Security/client_id.json');
+        $client->setRedirectUri('http://localhost:8100/googlelogin');
+        $client->addScope('profile');
+
+        $auth_url = $client->createAuthUrl();
+        return \FOS\RestBundle\View\View::create(["message" => filter_var($auth_url, FILTER_SANITIZE_URL)], Response::HTTP_OK);
+    }
+
+    /**
+     * Login with google
+     * Return :
+     * ['message' => "Login success",
+     * 'id' => user id,
+     * 'firstname' => use firstname,
+     * 'lastname' => user lastname,
+     * 'email' => user email,
+     * 'publicPicture' => user profilePicture]
+     *
+     * @ApiDoc(
+     *  description="Login",
+     *  section="1-Security"
+     * )
+     *
+     * @Rest\Post("/googlelogin")
+     */
+    public function postGoogleLoginAction(Request $request)
+    {
+
+        $client = new Google_Client();
+        $client->setAuthConfigFile('client_secrets.json');
+        $client->addScope('profile');
+
+        $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+        $client->setAccessToken($token);
+
+        $httpClient = $client->authorize();
+        $response = $httpClient->get('https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token='.$client->getAccessToken());
+
+        $google_oauth = new Google_Service_Oauth2($client);
+        $email = $google_oauth->userinfo->get()->email;
+        $name = $google_ouath->userinfo->get()->familyName;
+        $lastname = $google_ouath->userinfo->get()->givenName;
+        return \FOS\RestBundle\View\View::create(["response" => $response, "name" => $name, "email" => $email, "lastname" => $lastname], Response::HTTP_OK);
+        $em = $this->get('doctrine.orm.entity_manager');
+
+        $user = $em->getRepository('CoreBundle:User')
+                ->findOneByEmail($credentials->getLogin());
+
+        if (!$user) {
+            return $this->invalidCredentials();
+        }
+
+        $encoder = $this->get('security.password_encoder');
+        $isPasswordValid = $encoder->isPasswordValid($user, $credentials->getPassword());
+
+        if (!$isPasswordValid) {
+            if ($request->getSession()->get("connection_attemp") == null) {
+                $request->getSession()->set("connection_attemp", 1);
+            } else {
+                $request->getSession()->set("connection_attemp", $request->getSession()->get("connection_attemp") + 1);
+            }
+            if ($request->getSession()->get("connection_attemp") >= 5) {
+                return $this->replayAttackError();
+            }
+            return $this->invalidCredentials();
+        }
+        $request->getSession()->set("user_id", $user->getId());
+        $ret = [
+            'message' => "Login success",
+            'id' => $user->getId(),
+            'firstname' => $user->getFirstname(),
+            'lastname' => $user->getLastname(),
+            'email' => $user->getEmail(),
+            'profilePicture' => $user->getProfilePicture(),
+            'nbAchievements' => $user->getNbAchievements()
+        ];
+        return \FOS\RestBundle\View\View::create($ret, Response::HTTP_OK);
     }
 }
